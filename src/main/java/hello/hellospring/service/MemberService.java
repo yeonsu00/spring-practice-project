aec152133cdfa0ee592c6e2bea09412a34ad0da1
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2023-01-17 10:27:02 +0900

    회원 서비스 - 회원가입(중복확인), 전체 회원 조회, 회원 조회

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,44 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        validateDulicateMember(member); //같은 이름이 있는 중복 회원 X
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDulicateMember(Member member) {
+        /**
+         *result에 null이 아닌 값이 있으면 해당 로직 실행
+         *Optional이 아닐 땐 if(result != null) ...
+         */
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                     throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
