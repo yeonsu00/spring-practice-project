3ebf417c7cb4a85e4bb8886a509a60221648d669
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2023-01-20 00:02:10 +0900

    자바 코드로 직접 스프링 빈 등록

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,21 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean //스프링 빈을 입력
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
