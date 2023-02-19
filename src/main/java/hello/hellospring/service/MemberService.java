95855fd2093b04a1388ed6727f2a04968d03e786
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2023-02-19 21:57:54 +0900

    AOP가 필요한 상황

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -22,10 +22,18 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member) {
-        validateDulicateMember(member); //같은 이름이 있는 중복 회원 X
 
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+        try {
+            validateDulicateMember(member); //같은 이름이 있는 중복 회원 X
+
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timemMs = finish - start;
+            System.out.println("join = " + timemMs + "ms");
+        }
     }
 
     private void validateDulicateMember(Member member) {
@@ -43,7 +51,15 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId) {
